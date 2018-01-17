16e56a324621d4e3bef087c071e9043cd129396b
liiker <nx.liiker@gmail.com>
2018-01-17 14:58:32 +0800

    add db

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
-
 from flask import Flask
 from flask import render_template
+import sqlite3
 
 app = Flask(__name__)
+
+def load_data():
+    conn = sqlite3.connect('lesson.db')
+    coursor = conn.cursor()
+    rows = coursor.execute('select * from blog').fetchall()
+    return rows
 
 # 装饰器
 @app.route('/')
 @app.route('/<name>')
 def index(name='Eric'):
-    return render_template('home.html', name=name)
+    posts = load_data()
+    print posts
+    return render_template('home.html', name=name, posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
